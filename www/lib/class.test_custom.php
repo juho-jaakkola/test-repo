282b5f10d6ddd886ac80e5a94491befdc5e86020
Juho Jaakkola <juho.a.jaakkola@gmail.com>
2021-05-18 08:28:02 +0300

    Adds some dummy content for TestCustom class

--- www/lib/class.test_custom.php
+++ www/lib/class.test_custom.php
@@ -1,5 +1,19 @@
 <?php
 
 class TestCustom {
-
+    public function foo()
+    {
+      $exampleArray = [
+          'value1',
+          'value2',
+          'value3',
+          'value4',
+          'value5',
+          'value6',
+      ];
+    }
+  
+    public function bar()
+    {
+    }
 }
